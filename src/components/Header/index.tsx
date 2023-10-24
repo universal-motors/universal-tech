dca3f30307d71fc5516ad206a89d52533d096b22
razaahmad11 <razaqazhi@gmail.com>
2023-10-24 13:57:25 +0500

    fix: svg move in component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -29,50 +29,6 @@ export default function Header() {
         </div>
 
         <div className="nav-icons flex items-center lg_992:order-2 ms-auto">
-          <ul className="list-none menu-social mb-0">
-            {/* <li className="inline ms-1">
-              <Link href="#">
-                <span className="login-btn-primary">
-                  <span className="btn btn-sm btn-icon rounded-full bg-orange-600 hover:bg-orange-700 border-orange-600 hover:border-orange-700 text-white">
-                    <i className="uil uil-github"></i>
-                  </span>
-                </span>
-                <span className="login-btn-light">
-                  <span className="btn btn-sm btn-icon rounded-full bg-gray-50 hover:bg-gray-200 dark:bg-slate-900 dark:hover:bg-gray-700 hover:border-gray-100 dark:border-gray-700 dark:hover:border-gray-700">
-                    <i className="uil uil-github"></i>
-                  </span>
-                </span>
-              </Link>
-            </li> */}
-            {/* <li className="inline ms-1">
-              <Link href="#">
-                <span className="login-btn-primary">
-                  <span className="btn btn-sm btn-icon rounded-full bg-orange-600 hover:bg-orange-700 border-orange-600 hover:border-orange-700 text-white">
-                    <i className="uil uil-twitter"></i>
-                  </span>
-                </span>
-                <span className="login-btn-light">
-                  <span className="btn btn-sm btn-icon rounded-full bg-gray-50 hover:bg-gray-200 dark:bg-slate-900 dark:hover:bg-gray-700 hover:border-gray-100 dark:border-gray-700 dark:hover:border-gray-700">
-                    <i className="uil uil-twitter"></i>
-                  </span>
-                </span>
-              </Link>
-            </li> */}
-            {/* <li className="inline ms-1">
-              <Link href="#">
-                <span className="login-btn-primary">
-                  <span className="btn btn-sm btn-icon rounded-full bg-orange-600 hover:bg-orange-700 border-orange-600 hover:border-orange-700 text-white">
-                    <i className="uil uil-instagram"></i>
-                  </span>
-                </span>
-                <span className="login-btn-light">
-                  <span className="btn btn-sm btn-icon rounded-full bg-gray-50 hover:bg-gray-200 dark:bg-slate-900 dark:hover:bg-gray-700 hover:border-gray-100 dark:border-gray-700 dark:hover:border-gray-700">
-                    <i className="uil uil-instagram"></i>
-                  </span>
-                </span>
-              </Link>
-            </li> */}
-          </ul>
           <button
             type="button"
             className="collapse-btn inline-flex items-center ms-3 text-dark dark:text-white lg_992:hidden"
