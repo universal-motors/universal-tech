a9dddf89c3230505d104d4a0cd1a450c71366dd6
Mriakhan <mria.khan123@gmail.com>
2023-10-26 14:03:24 +0500

    fix: practice advise

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import img from "@/assets/images/cover.png";
 import AwardIcon from "@/assets/svg2/award";
 import CasesIcon from "@/assets/svg2/cases";
 import HappyIcon from "@/assets/svg2/happy";
+import SourceIcon from "@/assets/svg2/source";
 import ThreeIcon from "@/assets/svg2/three";
 import BagIcon from "@/assets/svgs/bag";
 import DigitalIcon from "@/assets/svgs/digital";
@@ -10,6 +11,9 @@ import FirmIcon from "@/assets/svgs/firmIcon";
 import { Box } from "@mui/material";
 import Image from "next/image";
 import Link from "next/link";
+import img2 from "@/assets/images/organize.png";
+import img3 from "@/assets/images/media.png";
+import img4 from "@/assets/images/work.png";
 export default function HomePage() {
   return (
     <Box sx={{ width: "100%" }}>
@@ -118,7 +122,7 @@ export default function HomePage() {
           <div>of Classical physics: Newtonian mechanics</div>
         </div>
         <div className="flex justify-center mt-[50px]">
-          <div className="shadow w-[230px] h-[209px]  flex flex-col items-center justify-center">
+          <div className="shadow w-[230px] h-[209px] ml-[30px]  flex flex-col items-center justify-center">
             <div className="w-[50px] h-[50px]">
               <ThreeIcon />
             </div>
@@ -128,7 +132,7 @@ export default function HomePage() {
             </div>
           </div>
 
-          <div className="shadow w-[230px] h-[209px] ml-[30px] flex flex-col items-center justify-center">
+          <div className="shadow w-[230px] h-[209px] ml-[30px] flex flex-col items-center justify-center ">
             <div>
               <HappyIcon />
             </div>
@@ -154,7 +158,82 @@ export default function HomePage() {
             <div className="text-[#737373] font-bold">CASES DONE</div>
           </div>
         </div>
-        <div></div>
+      </div>
+      <div className="text-center mt-[10%]">
+        <div className="font-bold text-[40px] mt-[50px]">Practice Advice</div>
+        <div>Problems trying to resolve the conflict between</div>
+        <div>
+          the two major realms of Classical physics: Newtonian mechanics
+        </div>
+      </div>
+      <div className="flex justify-center mt-[90px]">
+        <div className="ml-[30px] sm:mb-5">
+          <div className="shadow-lg w-[232px] h-[220px]">
+            <div className="font-bold text-[16] ml-[30px] mt-[30px]">
+              A single source <br />
+              of truth
+            </div>
+            <div className="text-[14px] font-medium  ml-[30px] mt-[30px] text-[#737373]">
+              Newton thought that <br />
+              light was made up of <br />
+              particles, but then it <br />
+              was discovered
+            </div>
+          </div>
+
+          <div className="">
+            <SourceIcon />
+          </div>
+        </div>
+        <div className=" ml-[30px] sm:mb-5">
+          <div className="shadow-lg w-[232px] h-[203px] ">
+            <div className="font-bold text-[16px] ml-[30px] mt-[30px] ">
+              Fastest way to <br />
+              organize
+            </div>
+            <div className="text-[14px]  ml-[30px] mt-[50px] text-[#737373]">
+              “Quantum mechanics” <br />
+              is the description of the <br />
+              behaviour of matter
+            </div>
+          </div>
+          <div>
+            <Image alt="img2" src={img2} />
+          </div>
+        </div>
+
+        <div className=" ml-[30px] sm:mb-5">
+          <div className="shadow-lg w-[232px] h-[203px]">
+            <div className="font-bold text-[16px] ml-[30px] mt-[30px]">
+              Fastest way to <br />
+              take action
+            </div>
+            <div className="text-[14px]  ml-[30px] mt-[50px] text-[#737373]">
+              They describe a <br />
+              universe consisting of <br />
+              bodies moving
+            </div>
+          </div>
+          <div>
+            <Image alt="alt1" src={img3} />
+          </div>
+        </div>
+        <div className="ml-[30px]">
+          <div className="shadow-lg w-[232px] h-[203px]  ">
+            <div className="font-bold text-[16px] ml-[30px] mt-[30px]">
+              Work better <br />
+              together
+            </div>
+            <div className=" text-[14px] ml-[30px] mt-[50px] text-[#737373]">
+              They finally obtained <br />
+              a consistent description <br />
+              of the behaviour
+            </div>
+          </div>
+          <div>
+            <Image alt="img" src={img4} />
+          </div>
+        </div>
       </div>
     </Box>
   );
