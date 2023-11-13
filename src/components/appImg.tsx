93f13047199b7ab4e8075a2513905bd0d658af9f
razaahmad11 <razaqazhi@gmail.com>
2023-11-13 17:35:51 +0500

    fix: portfolio-hover

--- src/components/appImg.tsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function AppImg() {
-  return <img src="https://www.expertizo.pk/assets/img/sesire.png" alt="" />;
-}
--- src/components/portfolio.tsx
+++ src/components/portfolio.tsx
@@ -131,7 +131,6 @@ export default function Portfolio() {
               }
             })}
           </div>
-          <div></div>
         </div>
       </div>
     </div>
